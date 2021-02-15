1059f5c78ac90a9985b2a61647809abc7cb05eae
Adam Mansfield <adam@lanamark.com>
2021-02-14 20:12:59 -0500

    Document VariableLiteral

--- VF2SubIso/src/main/java/infra/VariableLiteral.java
+++ VF2SubIso/src/main/java/infra/VariableLiteral.java
@@ -1,31 +1,55 @@
 package infra;
 
-public class VariableLiteral extends Literal {
+/**
+ * A variable literal to assert that a value of an attribute of a pair of vertices are the same.
+ *
+ * Example:
+ *   `player.city = team.city` is represented by:
+ *     new VariableLiteral(
+ *       "player", // vertexType_1
+ *       "team",   // vertexType_2
+ *       "city",   // attrName_1
+ *       "city")   // attrName_2
+ */
+public class VariableLiteral extends Literal
+{
+    //region --[Fields: Private]---------------------------------------
+    /** Vertex type of 1 */
+    private String vertexType_1;
+    /** Vertex type of 2 */
+    private String vertexType_2;
+    /** Attribute name of 1 */
+    private String attrName_1;
+    /** Attribute name of 2 */
+    private String attrName_2;
+    //endregion
 
-
-    String vertexType_1, vertexType_2, attrName_1, attrName_2;
-    public VariableLiteral(LiteralType t, String vertexType_1, String vertexType_2, String attrName_1, String attrName_2) {
-        super(t);
+    //region --[Constructors]------------------------------------------
+    /**
+     * Creates a VariableLiteral.
+     */
+    public VariableLiteral(String vertexType_1, String vertexType_2, String attrName_1, String attrName_2)
+    {
+        super(LiteralType.Variable);
 
         this.vertexType_1=vertexType_1;
         this.vertexType_2=vertexType_2;
         this.attrName_1=attrName_1;
         this.attrName_2=attrName_2;
     }
+    //endregion
 
-    public String getAttrName_1() {
-        return attrName_1;
-    }
+    //region --[Properties: Public]------------------------------------
+    /** Gets the attribute name of 1. */
+    public String getAttrName_1() { return attrName_1; }
 
-    public String getAttrName_2() {
-        return attrName_2;
-    }
+    /** Gets the attribute name of 2. */
+    public String getAttrName_2() { return attrName_2; }
 
-    public String getVertexType_1() {
-        return vertexType_1;
-    }
+    /** Gets the vertex type of 1. */
+    public String getVertexType_1() { return vertexType_1; }
 
-    public String getVertexType_2() {
-        return vertexType_2;
-    }
+    /** Gets the vertex type of 2. */
+    public String getVertexType_2() { return vertexType_2; }
+    //endregion
 }
