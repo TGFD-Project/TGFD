1db9f3852a57397abc7627003130af5307c38993
Adam Mansfield <adam@lanamark.com>
2021-03-12 01:34:33 -0500

    Add batch scrip to create all relevant IMDB snapshots

---
+++ Dataset/imdb/batch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Sync the dataset
+./sync.sh
+
+# Create snapshots for the following lists from 2017-10-13 to 2017-12-25.
+timestamp=171013
+./patch.sh actors $timestamp
+./patch.sh actresses $timestamp
+./patch.sh countries $timestamp
+./patch.sh distributors $timestamp
+./patch.sh genres $timestamp
+./patch.sh language $timestamp
+./patch.sh movies $timestamp
+./patch.sh ratings $timestamp
