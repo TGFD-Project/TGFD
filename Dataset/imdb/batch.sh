c262a8491473381e0910b161bb3a9872a9f12002
Adam Mansfield <me@adammansfield.com>
2021-03-14 11:25:40 -0400

    Add creation of RDF snapshots to batch.sh

--- Dataset/imdb/batch.sh
+++ Dataset/imdb/batch.sh
@@ -3,14 +3,27 @@
 # Sync the dataset
 ./sync.sh
 
-# Create snapshots for the following lists from 2017-10-13 to 2017-12-25.
-timestamp=171013
-./patch.sh actors $timestamp
-./patch.sh actresses $timestamp
-./patch.sh countries $timestamp
-./patch.sh directors $timestamp
-./patch.sh distributors $timestamp
-./patch.sh genres $timestamp
-./patch.sh language $timestamp
-./patch.sh movies $timestamp
-./patch.sh ratings $timestamp
+# Create snapshots for the following lists backwards from 2017-12-25 to 2014-02-14.
+# NB: IMDB database is missing diffs for 2014-02-07 and 2014-01-31 [2021-03-14]
+# So we cannot recreate snapshots back any further than 2014-02-14.
+last_valid_timestamp=140214
+./patch.sh --list=actors       --end=$last_valid_timestamp
+./patch.sh --list=actresses    --end=$last_valid_timestamp
+./patch.sh --list=countries    --end=$last_valid_timestamp
+./patch.sh --list=directors    --end=$last_valid_timestamp
+./patch.sh --list=distributors --end=$last_valid_timestamp
+./patch.sh --list=genres       --end=$last_valid_timestamp
+./patch.sh --list=language     --end=$last_valid_timestamp
+./patch.sh --list=movies       --end=$last_valid_timestamp
+./patch.sh --list=ratings      --end=$last_valid_timestamp
+
+# Grab the timestamps from the movies snapshots.
+# Assumes that all IMDB lists have the same timestamps (as they should from above).
+movie_snapshots=(./snapshots/list/movies-*.list)
+for movie_snapshot in "${movie_snapshots[@]}"; do
+  # Split by either '-' or '.' so that ./snapshots/list/movies-171222.list
+  # will be split into ['.', '/snapshots/list/movies', '171222', 'list'].
+  timestamp=`echo $movie_snapshot | awk -F'[-.]' '{print $3}'`
+
+  ./rdf.py $timestamp
+done
