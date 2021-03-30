c0e84698e6654c2ebbdd9b8e007534f3723615d6
mortez28 <morteza.alipur@gmail.com>
2021-03-30 02:12:15 -0400

    Added testSyntheticInc and testSyntheticGenerator for the synthetic datasets.

---
+++ VF2SubIso/src/test/java/testSyntheticGenerator.java
@@ -0,0 +1,86 @@
+import SyntheticGraph.SyntheticGenerator;
+import changeExploration.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import graphLoader.SyntheticLoader;
+import infra.TGFD;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class testSyntheticGenerator {
+
+    public static void main(String []args)
+    {
+        SyntheticLoader syntheticLoader=new SyntheticLoader(new ArrayList <TGFD>(), Collections.singletonList("C:\\Users\\admin\\Downloads\\social-a-graph.txt0.txt"));
+
+        SyntheticGenerator generator=new SyntheticGenerator(syntheticLoader);
+        HashMap <Integer, List <Change>> changeLogs = generator.generateSnapshot(3,0.04);
+
+        int t1=1;
+        for (int t2:changeLogs.keySet()) {
+            saveChanges(changeLogs.get(t2),t1,t2,"general");
+            t1=t2;
+        }
+    }
+
+    private static void saveChanges(List<Change> allChanges, int t1, int t2, String tgfdName)
+    {
+        System.out.println("Printing the changes: " + t1 +" -> " + t2);
+        int insertChangeEdge=0;
+        int insertChangeVertex=0;
+        int insertChangeAttribute=0;
+        int deleteChangeEdge=0;
+        int deleteChangeVertex=0;
+        int deleteChangeAttribute=0;
+        int changeAttributeValue=0;
+
+        for (Change c:allChanges) {
+            if(c instanceof EdgeChange)
+            {
+                if(c.getTypeOfChange()== ChangeType.deleteEdge)
+                    deleteChangeEdge++;
+                else if(c.getTypeOfChange()== ChangeType.insertEdge)
+                    insertChangeEdge++;
+            }
+            else if(c instanceof VertexChange)
+            {
+                if(c.getTypeOfChange()== ChangeType.deleteVertex)
+                    deleteChangeVertex++;
+                else if(c.getTypeOfChange()== ChangeType.insertVertex)
+                    insertChangeVertex++;
+            }
+            else if(c instanceof AttributeChange)
+            {
+                if(c.getTypeOfChange()== ChangeType.deleteAttr)
+                    deleteChangeAttribute++;
+                else if(c.getTypeOfChange()== ChangeType.insertAttr)
+                    insertChangeAttribute++;
+                else
+                    changeAttributeValue++;
+            }
+        }
+
+        final StringWriter sw =new StringWriter();
+        final ObjectMapper mapper = new ObjectMapper();
+        try
+        {
+            mapper.writeValue(sw, allChanges);
+            FileWriter file = new FileWriter("./changes_t"+t1+"_t"+t2+"_"+tgfdName+".json");
+            file.write(sw.toString());
+            file.close();
+            System.out.println("Successfully wrote to the file.");
+            sw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Total number of changes: " + allChanges.size());
+        System.out.println("Edges: +" + insertChangeEdge + " ** -" + deleteChangeEdge);
+        System.out.println("Vertices: +" + insertChangeVertex + " ** -" + deleteChangeVertex);
+        System.out.println("Attributes: +" + insertChangeAttribute + " ** -" + deleteChangeAttribute +" ** updates: "+ changeAttributeValue);
+    }
+}
---
+++ VF2SubIso/src/test/java/testSyntheticInc.java
@@ -0,0 +1,167 @@
+import BatchViolation.OptBatchTED;
+import IncrementalRunner.IncUpdates;
+import IncrementalRunner.IncrementalChange;
+import TGFDLoader.TGFDGenerator;
+import VF2Runner.VF2SubgraphIsomorphism;
+import changeExploration.Change;
+import changeExploration.ChangeLoader;
+import graphLoader.SyntheticLoader;
+import infra.*;
+import org.jgrapht.GraphMapping;
+import util.ConfigParser;
+import util.properties;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class testSyntheticInc
+{
+    public static void main(String []args) throws FileNotFoundException {
+
+        long wallClockStart=System.currentTimeMillis();
+
+        ConfigParser conf=new ConfigParser(args[0]);
+
+        System.out.println("Test Synthetic data incremental");
+
+        // Test whether we loaded all the files correctly
+
+        System.out.println(Arrays.toString(conf.getFirstTypesFilePath().toArray()) + " *** " + Arrays.toString(conf.getFirstDataFilePath().toArray()));
+        System.out.println(conf.getDiffFilesPath().keySet() + " *** " + conf.getDiffFilesPath().values());
+
+        //Load the TGFDs.
+        TGFDGenerator generator = new TGFDGenerator(conf.getPatternPath());
+        List<TGFD> allTGFDs=generator.getTGFDs();
+
+        //Create the match collection for all the TGFDs in the list
+        HashMap<String, MatchCollection> matchCollectionHashMap=new HashMap <>();
+        for (TGFD tgfd:allTGFDs) {
+            matchCollectionHashMap.put(tgfd.getName(),new MatchCollection(tgfd.getPattern(),tgfd.getDependency(),tgfd.getDelta().getGranularity()));
+        }
+
+        //Load the first timestamp
+        System.out.println("-----------Snapshot (1)-----------");
+        long startTime=System.currentTimeMillis();
+        LocalDate currentSnapshotDate=conf.getTimestamps().get(1);
+        SyntheticLoader synthetic = new SyntheticLoader(allTGFDs,conf.getFirstDataFilePath());
+
+        printWithTime("Load graph (1)", System.currentTimeMillis()-startTime);
+
+        // Now, we need to find the matches for each snapshot.
+        // Finding the matches...
+
+        for (TGFD tgfd:allTGFDs) {
+            VF2SubgraphIsomorphism VF2 = new VF2SubgraphIsomorphism();
+            System.out.println("\n###########"+tgfd.getName()+"###########");
+            Iterator<GraphMapping<Vertex, RelationshipEdge>> results= VF2.execute(synthetic.getGraph(), tgfd.getPattern(),false);
+
+            //Retrieving and storing the matches of each timestamp.
+            System.out.println("Retrieving the matches");
+            startTime=System.currentTimeMillis();
+            matchCollectionHashMap.get(tgfd.getName()).addMatches(currentSnapshotDate,results);
+            printWithTime("Match retrieval", System.currentTimeMillis()-startTime);
+        }
+
+        //Load the change files
+        Object[] ids=conf.getDiffFilesPath().keySet().toArray();
+        Arrays.sort(ids);
+        for (int i=0;i<ids.length;i++)
+        {
+            System.out.println("-----------Snapshot (" + ids[i] + ")-----------");
+
+            startTime=System.currentTimeMillis();
+            currentSnapshotDate=conf.getTimestamps().get((int)ids[i]);
+            ChangeLoader changeLoader=new ChangeLoader(conf.getDiffFilesPath().get(ids[i]));
+            List<Change> changes=changeLoader.getAllChanges();
+
+            printWithTime("Load changes ("+ids[i] + ")", System.currentTimeMillis()-startTime);
+            System.out.println("Total number of changes: " + changes.size());
+
+            // Now, we need to find the matches for each snapshot.
+            // Finding the matches...
+
+            startTime=System.currentTimeMillis();
+            System.out.println("Updating the graph");
+            IncUpdates incUpdatesOnDBpedia=new IncUpdates(synthetic.getGraph(),allTGFDs);
+            incUpdatesOnDBpedia.AddNewVertices(changes);
+
+            HashMap<String,ArrayList<String>> newMatchesSignaturesByTGFD=new HashMap <>();
+            HashMap<String,ArrayList<String>> removedMatchesSignaturesByTGFD=new HashMap <>();
+            HashMap<String,TGFD> tgfdsByName=new HashMap <>();
+            for (TGFD tgfd:allTGFDs) {
+                newMatchesSignaturesByTGFD.put(tgfd.getName(), new ArrayList <>());
+                removedMatchesSignaturesByTGFD.put(tgfd.getName(), new ArrayList <>());
+                tgfdsByName.put(tgfd.getName(),tgfd);
+            }
+            for (Change change:changes) {
+
+                //System.out.print("\n" + change.getId() + " --> ");
+                HashMap<String,IncrementalChange> incrementalChangeHashMap=incUpdatesOnDBpedia.updateGraph(change,tgfdsByName);
+                if(incrementalChangeHashMap==null)
+                    continue;
+                for (String tgfdName:incrementalChangeHashMap.keySet()) {
+                    newMatchesSignaturesByTGFD.get(tgfdName).addAll(incrementalChangeHashMap.get(tgfdName).getNewMatches().keySet());
+                    removedMatchesSignaturesByTGFD.get(tgfdName).addAll(incrementalChangeHashMap.get(tgfdName).getRemovedMatchesSignatures());
+                    matchCollectionHashMap.get(tgfdName).addMatches(currentSnapshotDate,incrementalChangeHashMap.get(tgfdName).getNewMatches());
+                }
+            }
+            for (TGFD tgfd:allTGFDs) {
+                matchCollectionHashMap.get(tgfd.getName()).addTimestamp(currentSnapshotDate,
+                        newMatchesSignaturesByTGFD.get(tgfd.getName()),removedMatchesSignaturesByTGFD.get(tgfd.getName()));
+                System.out.println("New matches ("+tgfd.getName()+"): " +
+                        newMatchesSignaturesByTGFD.get(tgfd.getName()).size() + " ** " + removedMatchesSignaturesByTGFD.get(tgfd.getName()).size());
+            }
+            printWithTime("Update and retrieve matches ", System.currentTimeMillis()-startTime);
+            //myConsole.print("#new matches: " + newMatchesSignatures.size()  + " - #removed matches: " + removedMatchesSignatures.size());
+        }
+
+        for (TGFD tgfd:allTGFDs) {
+
+            // Now, we need to find all the violations
+            //First, we run the Naive Batch TED
+            System.out.println("==========="+tgfd.getName()+"===========");
+
+            // we only need to run optimize method to find the violations
+            System.out.println("Running the optimized TED");
+
+            startTime=System.currentTimeMillis();
+            OptBatchTED optimize=new OptBatchTED(matchCollectionHashMap.get(tgfd.getName()),tgfd);
+            Set<Violation> allViolationsOptBatchTED=optimize.findViolations();
+            System.out.println("Number of violations (Optimized method): " + allViolationsOptBatchTED.size());
+            printWithTime("Optimized Batch TED", System.currentTimeMillis()-startTime);
+            if(properties.myProperties.saveViolations)
+                saveViolations("optimized",allViolationsOptBatchTED,tgfd);
+        }
+        printWithTime("Total wall clock time: ", System.currentTimeMillis()-wallClockStart);
+    }
+
+    private static void printWithTime(String message, long runTimeInMS)
+    {
+        System.out.println(message + " time: " + runTimeInMS + "(ms) ** " +
+                TimeUnit.MILLISECONDS.toSeconds(runTimeInMS) + "(sec) ** " +
+                TimeUnit.MILLISECONDS.toMinutes(runTimeInMS) +  "(min)");
+    }
+
+    private static void saveViolations(String path, Set<Violation> violations, TGFD tgfd)
+    {
+        try {
+            FileWriter file = new FileWriter(path +"_" + tgfd.getName() + ".txt");
+            file.write("***************TGFD***************\n");
+            file.write(tgfd.toString());
+            file.write("\n===============Violations===============\n");
+            for (Violation vio:violations) {
+                file.write(vio.toString() +
+                        "\n---------------------------------------------------\n");
+            }
+            file.close();
+            System.out.println("Successfully wrote to the file: " + path);
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
