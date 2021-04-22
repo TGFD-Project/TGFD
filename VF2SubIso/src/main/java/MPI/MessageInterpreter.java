1cde12cd84fb75d83d6e90dd128ba578d85d99e0
mortez28 <morteza.alipur@gmail.com>
2021-04-22 05:22:23 -0400

    Static class to interpret messages.

---
+++ VF2SubIso/src/main/java/MPI/MessageInterpreter.java
@@ -0,0 +1,23 @@
+package MPI;
+
+import infra.FocusNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageInterpreter {
+
+    public static List <FocusNode> getFocusNodes(String message)
+    {
+        List <FocusNode> allFocusNodes=new ArrayList <>();
+        for (String focusNodeMessage:message.split("\n")) {
+            String []res=focusNodeMessage.split("#");
+            if(res.length==3)
+            {
+                allFocusNodes.add(new FocusNode(res[0],res[1],Integer.parseInt(res[2])));
+            }
+        }
+        return allFocusNodes;
+    }
+
+}
