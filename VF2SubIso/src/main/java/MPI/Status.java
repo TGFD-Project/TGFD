6a92c5b73aff706cf2b59805a90fca0b12e6720c
mortez28 <morteza.alipur@gmail.com>
2021-04-22 05:21:15 -0400

    Status enum for parallel algorithm

---
+++ VF2SubIso/src/main/java/MPI/Status.java
@@ -0,0 +1,8 @@
+package MPI;
+
+public enum Status {
+    Coordinator_Wait_For_Workers_Status,
+    Coordinator_Wait_For_Workers_Results,
+    Coordinator_Assigns_jobs_To_Workers,
+    Coordinator_Is_Done
+}
