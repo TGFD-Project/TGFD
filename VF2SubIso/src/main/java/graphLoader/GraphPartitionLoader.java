82b1093984fcf4798a48bb03475aab99ab324e39
mortez28 <morteza.alipur@gmail.com>
2021-04-22 01:56:20 -0400

    Load fragmented graph based on a set of Focus nodes.

---
+++ VF2SubIso/src/main/java/graphLoader/GraphPartitionLoader.java
@@ -0,0 +1,16 @@
+package graphLoader;
+
+import infra.*;
+import org.jgrapht.Graph;
+
+import java.util.List;
+
+public class GraphPartitionLoader extends GraphLoader{
+
+    public GraphPartitionLoader(GraphLoader graphLoader, List<FocusNode> focusNodes) {
+        super();
+        Graph <Vertex, RelationshipEdge> subgraph= graphLoader.getGraph().getFragmentedGraph(focusNodes);
+        VF2DataGraph fragmentedGraph=new VF2DataGraph(subgraph);
+        setGraph(fragmentedGraph);
+    }
+}
