07bca2bd025f0e74bac0be51278952b8b85a1bbe
mortez28 <morteza.alipur@gmail.com>
2021-05-02 02:50:50 -0400

    Added CostEstimator class to compute cost (subgraph isomorphism and communication cost) based on partitioning.

---
+++ VF2SubIso/src/main/java/util/CostEstimator.java
@@ -0,0 +1,60 @@
+package util;
+
+import infra.DataVertex;
+import infra.TGFD;
+import infra.VF2DataGraph;
+import infra.Vertex;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class CostEstimator {
+
+    private VF2DataGraph graph;
+
+    public CostEstimator(VF2DataGraph graph)
+    {
+        this.graph=graph;
+    }
+
+    public int computeSubgraphIsomorphismCost(TGFD tgfd)
+    {
+        String centerNodeType=tgfd.getPattern().getCenterVertexType();
+        int patternSize=tgfd.getPattern().getSize();
+        int cost=0;
+        for (Vertex v:graph.getNodeMap().values()) {
+            if(v.getTypes().contains(centerNodeType))
+                cost+=patternSize;
+        }
+        return cost;
+    }
+
+    public HashMap<TGFD,Integer> computeSubgraphIsomorphismCost(List<TGFD> tgfds)
+    {
+        HashMap<TGFD, Integer> costs=new HashMap<>();
+        for (TGFD tgfd:tgfds) {
+            costs.put(tgfd, computeSubgraphIsomorphismCost(tgfd));
+        }
+        return costs;
+    }
+
+    public int computeCommunicationCost(TGFD tgfd, int assignedPartition, HashMap<String,Integer> partitionMapping)
+    {
+        String centerNodeType=tgfd.getPattern().getCenterVertexType();
+        HashSet<String> nodesToRetrieve=new HashSet<>();
+        for (String vertexURI:graph.getNodeMap().keySet()) {
+            DataVertex data_v=(DataVertex) graph.getNode(vertexURI);
+            if(data_v.getTypes().contains(centerNodeType)) {
+                List<Vertex> withinDiameter=graph.getVerticesWithinDiameter(data_v,tgfd.getPattern().getDiameter());
+                for (Vertex w:withinDiameter) {
+                    String uri=((DataVertex) w).getVertexURI();
+                    if(partitionMapping.containsKey(uri) && partitionMapping.get(uri)!=assignedPartition)
+                        nodesToRetrieve.add(uri);
+                }
+            }
+        }
+        return nodesToRetrieve.size();
+    }
+
+}
