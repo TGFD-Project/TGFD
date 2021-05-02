5fe7cc87b75fb3a8b7a96417f8bb753b876f8427
mortez28 <morteza.alipur@gmail.com>
2021-05-02 02:52:58 -0400

    Simple graph partitioning algorithm. Randomly and Evenly assigns nodes to partitions.

--- VF2SubIso/src/main/java/Partitioner/SimpleGraphPartitioner.java
+++ VF2SubIso/src/main/java/Partitioner/SimpleGraphPartitioner.java
@@ -1,14 +1,37 @@
 package Partitioner;
 
 import graphLoader.GraphLoader;
+import infra.VF2DataGraph;
+
+import java.util.HashMap;
 
 public class SimpleGraphPartitioner{
 
-    private GraphLoader loader;
+    private VF2DataGraph graph;
 
-    public SimpleGraphPartitioner(GraphLoader loader, int numberOfPartitions)
+
+    public SimpleGraphPartitioner(VF2DataGraph graph)
     {
-        this.loader=loader;
+        this.graph=graph;
+    }
+
+    public HashMap<String,Integer> partition(int numberOfPartitions)
+    {
+        if(numberOfPartitions<1)
+            return null;
+        HashMap<String,Integer> mapping=new HashMap<>();
+        int numberOfVertices=graph.getSize();
+        int partitionSize=numberOfVertices/numberOfPartitions;
+        int i=0, partitionID=1;
+        for (String vertexURI:graph.getNodeMap().keySet()) {
+            if(++i>partitionSize)
+            {
+                i=0;
+                partitionID++;
+            }
+            mapping.put(vertexURI,partitionID);
+        }
+        return mapping;
     }
 
 }
