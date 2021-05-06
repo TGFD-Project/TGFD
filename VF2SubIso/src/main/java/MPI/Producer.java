2261f275cd392ec2c029e45effdf0d75b5303e29
mortez28 <morteza.alipur@gmail.com>
2021-05-06 12:39:49 -0400

    Added Producer class to send message to the ActiveMQ broker

---
+++ VF2SubIso/src/main/java/MPI/Producer.java
@@ -0,0 +1,72 @@
+package MPI;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import util.ConfigParser;
+
+import javax.jms.*;
+
+public class Producer {
+
+    private ActiveMQConnectionFactory connectionFactory;
+    private String url;
+    private Connection connection=null;
+    private Session session=null;
+
+    public Producer()
+    {
+        url= ConfigParser.ActiveMQBrokerURL;
+    }
+
+    public void connect()
+    {
+        try
+        {
+            connectionFactory = new ActiveMQConnectionFactory(url);
+            connectionFactory.setUserName(ConfigParser.ActiveMQUsername);
+            connectionFactory.setPassword(ConfigParser.ActiveMQPassword);
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void send(String dstQueue, String msg)
+    {
+        try
+        {
+            if(connection==null || session ==null) {
+                System.out.println("Connection is not established to the broker...");
+                return;
+            }
+
+            Destination destination = session.createQueue(dstQueue);
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage(msg);
+            producer.send(message);
+            System.out.println("Message sent to '" + dstQueue + "' successfully");
+            producer.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void close()
+    {
+        try
+        {
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
