5640562a96b6ef0210e13a4febccec34d528f9cd
mortez28 <morteza.alipur@gmail.com>
2021-05-06 12:39:29 -0400

    Added Consumer class to receive message from ActiveMQ broker

---
+++ VF2SubIso/src/main/java/MPI/Consumer.java
@@ -0,0 +1,81 @@
+package MPI;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import util.ConfigParser;
+
+import javax.jms.*;
+
+public class Consumer {
+
+    private ActiveMQConnectionFactory connectionFactory;
+    private String url;
+    private Connection connection=null;
+    private Session session=null;
+    private MessageConsumer consumer=null;
+
+    public Consumer()
+    {
+        url= ConfigParser.ActiveMQBrokerURL;
+    }
+
+    public void connect(String queueName)
+    {
+        try
+        {
+            connectionFactory = new ActiveMQConnectionFactory(url);
+            connectionFactory.setUserName(ConfigParser.ActiveMQUsername);
+            connectionFactory.setPassword(ConfigParser.ActiveMQPassword);
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue(queueName);
+            consumer = session.createConsumer(destination);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String receive()
+    {
+        try
+        {
+            if(connection==null || session ==null) {
+                System.out.println("Connection is not established to the broker...");
+                return null;
+            }
+            Message message = consumer.receive();
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                return textMessage.getText();
+            }
+            else
+            {
+                System.out.println("Message received, but not an instance of text message.");
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public void close()
+    {
+        try
+        {
+            consumer.close();
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
