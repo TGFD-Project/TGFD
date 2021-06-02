9a721305c0b4a790cdb92f277dae8bad3a7594bb
mortez28 <morteza.alipur@gmail.com>
2021-06-01 23:22:38 -0400

    GFD Conversion class

---
+++ VF2SubIso/src/test/java/testGFDMiner.java
@@ -0,0 +1,44 @@
+import graphLoader.DBPediaLoader;
+import graphLoader.IMDBLoader;
+import infra.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class testGFDMiner {
+
+    public static void main(String []args)
+    {
+        ArrayList <String> paths=new ArrayList <>();
+        paths.add(args[0]);
+        DBPediaLoader imdb = new DBPediaLoader(null,new ArrayList <String>(),paths);
+
+        StringBuilder sb=new StringBuilder();
+        for (Vertex v:imdb.getGraph().getGraph().vertexSet()) {
+            DataVertex data_v=(DataVertex) v;
+            String v_type=data_v.getTypes().stream().findFirst().orElse("");
+            sb.append("L").append("\t").append(data_v.getVertexURI()).append("\t").append(v_type).append("\n");
+            for (Attribute attr:data_v.getAllAttributesList()) {
+                if(!attr.getAttrName().equals("uri"))
+                    sb.append("A").append("\t").append(data_v.getVertexURI()).append("\t").append(attr.getAttrName()).append("\t").append(attr.getAttrValue()).append("\n");
+            }
+            for (RelationshipEdge edge:imdb.getGraph().getGraph().outgoingEdgesOf(v)) {
+                DataVertex out_v=(DataVertex) edge.getTarget();
+                sb.append("E").append("\t").append(data_v.getVertexURI()).append("\t").append(out_v.getVertexURI()).append("\t").append(edge.getLabel()).append("\n");
+            }
+        }
+
+        try {
+            FileWriter file = new FileWriter(args[1]);
+            file.write(sb.toString());
+            file.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+}
