2cae2521a5937a970331c3e6a78392ff95f2a04b
mortez28 <morteza.alipur@gmail.com>
2021-07-04 16:19:33 -0400

    Refactored.

--- VF2SubIso/src/test/java/testBasicParallel.java
+++ VF2SubIso/src/test/java/testBasicParallel.java
@@ -1,17 +1,17 @@
-import ParalleRunner.SimpleCoordinator;
-import ParalleRunner.SimpleWorker;
+import ParalleRunner.BasicCoordinator;
+import ParalleRunner.BasicWorker;
 import util.Config;
 
 import java.io.FileNotFoundException;
 
-public class testParallelIMDB {
+public class testBasicParallel {
 
     public static void main(String []args) throws FileNotFoundException {
         Config.parse(args[0]);
         if(Config.nodeName.equalsIgnoreCase("coordinator"))
         {
 
-            SimpleCoordinator simpleCoordinator =new SimpleCoordinator();
+            BasicCoordinator simpleCoordinator =new BasicCoordinator();
             simpleCoordinator.start();
             simpleCoordinator.assignJob(Config.jobs);
             simpleCoordinator.waitForResults();
@@ -19,7 +19,7 @@ public class testParallelIMDB {
         else
         {
             System.out.println("Worker '"+ Config.nodeName+"' is starting...");
-            SimpleWorker worker=new SimpleWorker();
+            BasicWorker worker=new BasicWorker();
             worker.start();
         }
     }
---
+++ VF2SubIso/src/test/java/testMediumParallel.java
@@ -0,0 +1,28 @@
+import ParalleRunner.BasicCoordinator;
+import ParalleRunner.BasicWorker;
+import ParalleRunner.MediumCoordinator;
+import ParalleRunner.MediumWorker;
+import util.Config;
+
+import java.io.FileNotFoundException;
+
+public class testMediumParallel {
+
+    public static void main(String []args) throws FileNotFoundException {
+        Config.parse(args[0]);
+        if(Config.nodeName.equalsIgnoreCase("coordinator"))
+        {
+
+            MediumCoordinator mediumCoordinator =new MediumCoordinator();
+            mediumCoordinator.start();
+            mediumCoordinator.assignJob(Config.jobs);
+            mediumCoordinator.waitForResults();
+        }
+        else
+        {
+            System.out.println("Worker '"+ Config.nodeName+"' is starting...");
+            MediumWorker worker=new MediumWorker();
+            worker.start();
+        }
+    }
+}
