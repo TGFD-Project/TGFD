e00f9ab67d2523ee6bb2184500c305290eb6e270
mortez28 <morteza.alipur@gmail.com>
2021-07-14 03:37:00 -0400

    Add label.

--- VF2SubIso/src/main/java/Infra/SimpleEdge.java
+++ VF2SubIso/src/main/java/Infra/SimpleEdge.java
@@ -2,17 +2,19 @@ package Infra;
 
 public class SimpleEdge
 {
-    private String src,dst;
-    public SimpleEdge(String src, String dst)
+    private String src, dst, label;
+    public SimpleEdge(String src, String dst, String label)
     {
         this.src=src;
         this.dst=dst;
+        this.label=label;
     }
 
     public SimpleEdge(RelationshipEdge edge)
     {
         this.src=((DataVertex)edge.getSource()).getVertexURI();
         this.dst=((DataVertex)edge.getTarget()).getVertexURI();
+        this.label=edge.getLabel();
     }
 
     public String getSrc() {
@@ -21,5 +23,9 @@ public class SimpleEdge
 
     public String getDst() {
         return dst;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
