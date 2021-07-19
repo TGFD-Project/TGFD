825e426d450604a3e77bb1541715ba91bfe846fa
mortez28 <morteza.alipur@gmail.com>
2021-07-19 00:13:06 -0400

    Minor bugs fixed.

--- VF2SubIso/src/main/java/Infra/VF2DataGraph.java
+++ VF2SubIso/src/main/java/Infra/VF2DataGraph.java
@@ -41,10 +41,6 @@ public class VF2DataGraph implements Serializable {
             graph.addVertex(v);
             nodeMap.put(v.getVertexURI(),v);
         }
-//        else
-//        {
-//            System.out.println("Node already existed, Vertex URI: " + v.getVertexURI());
-//        }
     }
 
     public Vertex getNode(String vertexURI)
@@ -443,7 +439,6 @@ public class VF2DataGraph implements Serializable {
         }
         return fragmentedGraph;
     }
-
 
     public void updateGraphByAttribute(DataVertex v1, Attribute attribute)
     {
