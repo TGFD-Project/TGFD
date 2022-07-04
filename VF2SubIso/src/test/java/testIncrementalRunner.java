a5495252695bd06db57d90047b8d5e2db8f3328d
Morteza <morteza.alipur@gmail.com>
2022-07-04 14:26:39 -0400

    Added a generic incremental runner for any dataset based on the testRunner class (dataset name to be determined in the config file)

---
+++ VF2SubIso/src/test/java/testIncrementalRunner.java
@@ -0,0 +1,15 @@
+import Util.Config;
+import Util.testRunner;
+
+import java.io.FileNotFoundException;
+
+public class testIncrementalRunner
+{
+    public static void main(String []args) throws FileNotFoundException {
+        Config.parse(args[0]);
+        testRunner runner = new testRunner();
+        runner.load();
+        runner.run();
+    }
+}
+
