6977dbe9fb1e3b155423f1f6b3e4136a4ea2b491
juzitadi <Lejia.Zhang17@gmail.com>
2022-08-02 21:42:14 -0400

    Add saveDoses() and saveClusters() methods for graph plotting in Python.

--- VF2SubIso/src/test/java/testPDDClusters.java
+++ VF2SubIso/src/test/java/testPDDClusters.java
@@ -5,6 +5,8 @@ import PDD.Record;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 import static java.util.stream.Collectors.toSet;
@@ -12,11 +14,13 @@ import static java.util.stream.Collectors.toSet;
 
 public class testPDDClusters {
     public static void main(String[] args) {
-        //TODO:read match from TGFD match results
+        //read match from TGFD match results
         HashMap<String, double[]> patientsDose_fromFile=readDosageFromFile("/Users/lexie/Desktop/Master_Project/Violations/P1612/match_p1612.txt");
+        saveDoses("/Users/lexie/Desktop/Master_Project/Clusters/doses",patientsDose_fromFile);
         HashMap<String,HashMap<String,Double>> patientsBio_fromFile = readBioFromFile("/Users/lexie/Desktop/Master_Project/Violations/P1612/match_p1612.txt");
 
         List<String> patients =getPatients(patientsDose_fromFile);
+        System.out.println("Patients size is "+patients.size());
 //
 //        for(String patient:patients){
 //            System.out.println("patient is"+patient);
@@ -31,7 +35,7 @@ public class testPDDClusters {
 //                System.out.println("attribute"+r.getKey()+"with value"+r.getValue());
 //            }
 //        }
-        //TODO:create patients records with URI and Features
+        //create patients records with URI and Features
         List<Record> records = datasetWithTaggedPatients(patientsWithTags);
 //        for(Record r:records){
 //            System.out.println(r.toString());
@@ -45,25 +49,82 @@ public class testPDDClusters {
     }
 
     public static void computeKMeansClustering(List<Record> records){
-        //TODO:form k-means Clustering
+        HashMap<String,HashMap<String,String>> clusters_collection = new HashMap<>();
+        //form k-means Clustering
         Map<Centroid,List<Record>> clusters = KMeans.fit(records,7,new EuclideanDistance(),1000);
+
         // Printing the cluster configuration
+        String number;
         clusters.forEach((key, value) -> {
             System.out.println("-------------------------- CLUSTER ----------------------------");
 
             // Sorting the coordinates to see the most significant tags first.
-//            System.out.println(sortedCentroid(key));
+            System.out.println(key);
             //TODO: change stream
             String members = String.join(", ", value.stream().map(Record::getUri).collect(toSet()));
             System.out.print(members);
             System.out.println();
             System.out.println();
+
+            HashMap<String,String> temp_attributes = new HashMap<>();
+
+            temp_attributes.put("centroid",key.toString());
+            temp_attributes.put("members",members);
+
+            clusters_collection.put("Cluster"+key,temp_attributes);
+
         });
+        saveClusters("/Users/lexie/Desktop/Master_Project/Clusters/cluster",clusters_collection);
+
     }
 
-    public static HashMap<String, HashMap<String,Double>> readBioFromFile(String path){
+    public static void saveClusters(String path,HashMap<String,HashMap<String,String>> clusters_collection) {
+        int num=1;
+        try {
+            FileWriter file1 = new FileWriter(path + "_"+"1612"+".txt");
+            for(Map.Entry<String,HashMap<String,String>> i:clusters_collection.entrySet()){
+//                System.out.println(i.getKey());
+                file1.write(num+".");
+                HashMap<String,String> temp = i.getValue();
+                for(Map.Entry<String,String> r:temp.entrySet()){
+//                    System.out.println(r.getKey()+r.getValue());
+                    file1.write(r.getKey()+r.getValue()+"\n");
+                }
+                num++;
+            }
+            System.out.println("Successfully write file"+path + "_"+"1612"+".txt");
+            file1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void saveDoses(String path,HashMap<String, double[]> patient_collection){
+        int num=1;
+        try {
+            FileWriter file1 = new FileWriter(path + "_"+"1612"+".txt");
+
+            for(Map.Entry<String,double[]> e:patient_collection.entrySet()){
+                String dose="dose:";
+                String patient = e.getKey();
+                file1.write(num+"."+patient+"\n");
+                double[] doses = e.getValue();
+                for(double i:doses){
+                    dose = dose+ i +",";
+                }
+                file1.write(dose+"\n");
+                num++;
+            }
+            System.out.println("Successfully write file"+path + "_"+"1612"+".txt");
+            file1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        public static HashMap<String, HashMap<String,Double>> readBioFromFile(String path){
         HashMap<String, HashMap<String,Double>> patient_collection = new HashMap<>();
-        HashMap<String,Double> bio_collection = new HashMap<>();
         String temp_age;
         String temp_gender;
         String temp;
@@ -139,14 +200,17 @@ public class testPDDClusters {
                         temp = data.substring(data.indexOf("admission_2.uri: "), data.indexOf(",admission_1.age:"));
                         patient = temp.substring(temp.lastIndexOf(" ")+1);
 
-
-                        temp_interval = data.substring(data.indexOf("[Interval{start= "), data.indexOf("}], signatureX="));
+                        //TODO: improve for multiple intervals
+                        temp_interval = data.substring(data.indexOf("[Interval{start= "), data.indexOf("}"));
 //                        System.out.println("Interval is" + temp_interval);
 
                         start_date = Integer.parseInt(temp_interval.substring(19, temp_interval.lastIndexOf(",")));
                         end_date = Integer.parseInt(temp_interval.substring(temp_interval.indexOf(", end= t_")+9));
 
                         temp_signatureY = data.substring(data.indexOf("signatureY=")+11, data.indexOf("mg"));
+                        if(temp_signatureY.contains("-")){
+                            temp_signatureY = temp_signatureY.substring(0,temp_signatureY.indexOf('-'));
+                        }
                         signatureY = Double.parseDouble(temp_signatureY);
 //                        System.out.println("signature" + "for patient"+patient+"is"+ signatureY);
                         putInDoseHashMap(patient,start_date,end_date,patient_collection,signatureY);
